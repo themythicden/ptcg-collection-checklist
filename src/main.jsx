b75bfcc56414b36ff46e852635799b1217951eba
themythicden <themythicden@gmail.com>
2025-07-05 14:33:30 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import ChecklistPage from './ChecklistPage';
 import './index.css';
-import PrintableList from './PrintableList';
+import PrintPage from './pages/PrintPage';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
