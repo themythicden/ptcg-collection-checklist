482943d48adfdcd3047209168a1b50955c5ddf84
themythicden <themythicden@gmail.com>
2025-07-11 00:46:12 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
-import ChecklistPage from './ChecklistPage';
+//import LandingPage from './LandingPage';
+//import ChecklistPage from './ChecklistPage';
 import './index.css';
+import LandingPage from './pages/LandingPage';
+import ChecklistPage from './pages/ChecklistPage';
 import PrintPage from './pages/PrintPage';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
