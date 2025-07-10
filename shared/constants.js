94b2ecd88a3d2f1a717488fdeae98d75ce173c29
themythicden <themythicden@gmail.com>
2025-07-10 19:19:34 +0200

    Create constants.js

---
+++ shared/constants.js
@@ -0,0 +1,83 @@
+const BASE_COUNTS = {
+  DestinedRivals: 182,
+  JourneyTogether: 159,
+  PrismaticEvolutions: 131,
+  SurgingSparks: 191,
+  StellarCrown: 142,
+  ShroudedFable: 64,
+  TwilightMasquerade: 167,
+  TemporalForces: 162,
+  PaldeanFates: 91,
+  ParadoxRift: 182,
+  "151": 165,
+  ObsidianFlames: 197,
+  PaldeaEvolved: 193,
+  ScarletViolet: 198,
+  SilverTempest: 195,
+  SteamSiege: 114
+};
+
+const MASTER_COUNTS = {
+  DestinedRivals: 244,
+  JourneyTogether: 190,
+  PrismaticEvolutions: 180,
+  SurgingSparks: 252,
+  StellarCrown: 175,
+  ShroudedFable: 95,
+  TwilightMasquerade: 226,
+  TemporalForces: 218,
+  PaldeanFates: 123,
+  ParadoxRift: 266,
+  "151": 207,
+  ObsidianFlames: 230,
+  PaldeaEvolved: 279,
+  ScarletViolet: 258,
+  SilverTempest: 215,
+  SteamSiege: 116
+};
+
+const SET_CODES = {
+  DestinedRivals: 'sv10',
+  JourneyTogether: 'sv9',
+  PrismaticEvolutions: 'sv8pt5',
+  SurgingSparks: 'sv8',
+  StellarCrown: 'sv7',
+  ShroudedFable: 'sv6pt5',
+  TwilightMasquerade: 'sv6',
+  TemporalForces: 'sv5',
+  PaldeanFates: 'sv4pt5',
+  ParadoxRift: 'sv4',
+  "151": 'sv3pt5',
+  ObsidianFlames: 'sv3',
+  PaldeaEvolved: 'sv2',
+  ScarletViolet: 'sv1',
+  SilverTempest: 'swsh12',
+  SteamSiege: 'xy11'
+};
+
+const SET_NAME_MAP = Object.fromEntries(
+  Object.entries(SET_CODES).map(([name, code]) => [code, name])
+);
+
+const SHEET_NAMES = Object.fromEntries(
+  Object.keys(SET_CODES).map((key) => [key, key])
+);
+
+const DISPLAY_NAMES = {
+  ScarletViolet: 'Scarlet & Violet',
+  "151": '151'
+};
+
+function formatSetName(name) {
+  return DISPLAY_NAMES[name] || name.replace(/([A-Z])/g, ' $1').trim();
+}
+
+module.exports = {
+  BASE_COUNTS,
+  MASTER_COUNTS,
+  SET_CODES,
+  SET_NAME_MAP,
+  SHEET_NAMES,
+  DISPLAY_NAMES,
+  formatSetName
+};
