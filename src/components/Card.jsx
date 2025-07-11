f43bd0ca0a4cc362e840be8d87f50c39d08ddce6
themythicden <themythicden@gmail.com>
2025-07-12 01:35:01 +0200

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -119,7 +119,7 @@ function Card({ card, mode, onCheckboxChange }) {
       } else {
         add('holoFoil', 'Holo Foil');
       }
-    } else if (isEvolutions) {
+    } else if (isXY) {
         if (isCommonOrUncommon){
           add('standard', 'Standard');
           add('reverseHolo', 'Reverse Holo');
